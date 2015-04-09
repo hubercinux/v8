ab61db096afece8057327676d5e532e5ca386c6b
Javier huber Salazar Carlos <javier@salazarcarlos.com>
2015-04-09 16:07:11 -0500

    Quitando espacios en blanco al inicio

--- idea/idea.py
+++ idea/idea.py
@@ -20,9 +20,6 @@
 ##############################################################################
 
 
-
-
-
 from openerp import models, fields, api, _
 
 from openerp.exceptions import except_orm, Warning, RedirectWarning
