630b37ac630c96f21e0b04762b36777ff3e8e18d
Javier Huber Salazar Carlos <javier@salazarcarlos.com>
2015-04-17 02:01:03 -0500

    Añadiendo nueva funcion amount_to_text_pos pos agregado en base_translate

--- pos_extend/static/src/js/amount_text.js
+++ pos_extend/static/src/js/amount_text.js
@@ -5,7 +5,8 @@ function odoo_amount_text(instance,module){
     module.Order = module.Order.extend({
         amount_text: function(){
             var model_tex = new instance.web.Model("ir.translation");
-            model_tex.call('amount_to_text',{'nbr':this.getTotalTaxIncluded(),'lang':'hn','currency':'LEMPIRAS'}).then(function(monto2text){
+            //console.log("ERROR1", this.pos.currency.name.toLowerCase().substring(0, 2) );
+            model_tex.call('amount_to_text_pos',{'nbr':this.getTotalTaxIncluded(),'lang':this.pos.currency.name.toLowerCase().substring(0, 2),'currency':''}).then(function(monto2text){
                 //console.log("ERROR1", monto2text );
                 $('#montotext').html('SON: '+ monto2text);                          
             });
