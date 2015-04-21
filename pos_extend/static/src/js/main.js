47f9c8f841d35625821aa808c7ddb22c28b30254
Javier huber Salazar Carlos <javier@salazarcarlos.com>
2015-04-21 17:51:56 -0500

    corrigiendo el order de importar las funciones

--- pos_extend/static/src/js/main.js
+++ pos_extend/static/src/js/main.js
@@ -2,7 +2,8 @@ openerp.pos_extend = function(instance){
 
     var module = instance.point_of_sale;
 
-    odoo_amount_text(instance,module);
     multicurrency_payment(instance,module);
+    odoo_amount_text(instance,module);
+    
 
 };
