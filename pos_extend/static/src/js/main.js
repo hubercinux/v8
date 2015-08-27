6fa227b68ec256ae90052d604638aadeec2c859e
Javier huber Salazar Carlos <javier@salazarcarlos.com>
2015-08-27 17:06:10 -0500

    comentado

--- pos_extend/static/src/js/main.js
+++ pos_extend/static/src/js/main.js
@@ -2,8 +2,6 @@ openerp.pos_extend = function(instance){
 
     var module = instance.point_of_sale;
 
-    multicurrency_payment(instance,module);
     odoo_amount_text(instance,module);
-    
-
+    //multicurrency_payment(instance,module); // comentado para multimoneda error impresion
 };
